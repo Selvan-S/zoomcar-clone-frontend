ddd457deb049fba15b0d4807daf3000d32522978
Selvan-S <selvan0023@gmail.com>
2024-06-24 23:15:06 +0530

    added netlify toml

--- src/Components/Common/PageNotFound.jsx
+++ src/Components/Common/PageNotFound.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="mx-auto text-5xl text-primary mt-10">
+      404: Page Not Found
+    </div>
+  );
 }
 
 export default PageNotFound;
